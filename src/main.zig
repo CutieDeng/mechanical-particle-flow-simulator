f683ed5a2b0793036a29fa5744bfd16ef260fec9
Cutie Deng <CutieDeng@iCloud.com>
2025-08-30 02:11:46 +0800

    (INIT "impl simple simulator")

---
+++ src/main.zig
@@ -0,0 +1,42 @@
+//! By convention, main.zig is where your main function lives in the case that
+//! you are building an executable. If you are making a library, the convention
+//! is to delete this file and start with root.zig instead.
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const a = gpa.allocator();
+    defer _ = gpa.deinit();
+    var random_raw = std.Random.DefaultPrng.init (@bitCast(std.time.milliTimestamp()));
+    const random = random_raw.random();
+    const row = 258;
+    const col = 258;
+    const ms = try lib.MatrixState.init (row, col, a);
+    var machine = lib.Machine {
+        .allocator = a,
+        .random = random,
+        .matrix_state = ms,
+        .visitor = .{},
+        .activated = .{},
+        .todo_list = .{},
+        .todo_offset = undefined,
+    };
+    defer machine.deinit ();
+    machine.visitor = try std.DynamicBitSetUnmanaged.initEmpty(a, row * col);
+    try do_test (&machine);
+}
+
+pub fn do_test (m: *lib.Machine) !void {
+    const STEP_CNT = 256 * 256 * 200;
+    const start = std.time.milliTimestamp();
+    for (0..STEP_CNT) |i| {
+        _ = i; // autofix
+        try m.step ();
+    }
+    const end = std.time.milliTimestamp();
+    std.debug.print ("cost milli time: {d} ms.\n", .{ end - start });
+}
+
+const std = @import("std");
+
+/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
+const lib = @import("automaton_0380_lib");
---
+++ src/root.zig
@@ -0,0 +1,135 @@
+//! By convention, root.zig is the root source file when making a library. If
+//! you are making an executable, the convention is to delete this file and
+//! start with main.zig instead.
+const std = @import("std");
+const testing = std.testing;
+
+pub const State = struct {
+    @"0": i8,
+
+    const This = State;
+
+    pub inline fn val (s: This) i8 {
+        return s.@"0";
+    }
+
+    pub fn random_init (r: std.Random) This {
+        const raw_value = r.int(i8);
+        const value = std.math.clamp(raw_value, -127, 127);
+        return .{ value };
+    }
+
+    pub fn is_activated (s: This, other: i8) bool {
+        return s.val() >= other;
+    }
+    pub fn revert (s: *This) void {
+        s.@"0" = - s.@"0";
+    }
+};
+
+pub const MatrixState = struct {
+    data: [*]State,
+    row: usize,
+    col: usize,
+    data_row: usize,
+    data_col: usize,
+    const This = @This();
+    const sub_row = 8;
+    const sub_col = 16;
+    const sub_size = sub_row * sub_col;
+    pub fn unsafe_ref (m: This, r: usize, c: usize) *State {
+        const r1 = std.math.divFloor (usize, r, sub_row) catch unreachable;
+        const r2 = std.math.rem (usize, r, sub_row) catch unreachable;
+        const c1 = std.math.divFloor (usize, c, sub_col) catch unreachable;
+        const c2 = std.math.rem (usize, c, sub_col) catch unreachable;
+        const sub_offest = r2 * sub_col + c2;
+        const offset = r1 * m.col + c1;
+        return &m.data [offset * sub_size + sub_offest];
+    }
+    pub fn ref (m: This, r: usize, c: usize) ?*State {
+        if (r >= m.data_row or c >= m.data_col) {
+            return null;
+        }
+        return unsafe_ref(m, r, c);
+    }
+    pub fn init (r: usize, c: usize, a: std.mem.Allocator) !MatrixState {
+        const rtop = std.math.divCeil(usize, r, sub_row) catch unreachable;
+        const ctop = std.math.divCeil (usize, c, sub_col) catch unreachable;
+        const sz = rtop * ctop * sub_size;
+        const d = try a.alloc (State, sz);
+        return .{
+            .data = d.ptr,
+            .data_row = r,
+            .data_col = c,
+            .row = rtop,
+            .col = ctop, 
+        };
+    }
+    pub fn deinit (m: MatrixState, a: std.mem.Allocator) void {
+        a.free(m.data[0..m.row * m.col * sub_size]);
+    }
+};
+
+const Index = struct {
+    row: usize,
+    col: usize,
+};
+
+pub const Machine = struct {
+    allocator: std.mem.Allocator,
+    random: std.Random,
+    matrix_state: MatrixState,
+    visitor: std.DynamicBitSetUnmanaged,
+    activated: std.DynamicBitSetUnmanaged,
+    todo_list: std.ArrayListUnmanaged(Index),
+    todo_offset: usize,
+    pub fn step (m: *Machine) !void {
+        const raw_th : f32 = m.random.floatNorm(f32) * 255 - 128;
+        const raw_th2 : f32 = std.math.clamp (raw_th, -127, 127);
+        const th : i8 = @intFromFloat(raw_th2);
+        const index_row_select = m.random.intRangeLessThan(usize, 0, m.matrix_state.data_row); 
+        const index_col_select = m.random.intRangeLessThan(usize, 0, m.matrix_state.data_col);
+        m.visitor.unsetAll();
+        m.todo_offset = 0;
+        m.todo_list.clearRetainingCapacity();
+        m.visitor.set(index_row_select * m.matrix_state.data_col + index_col_select);
+        try m.todo_list.append(m.allocator, .{ .row = index_row_select, .col = index_col_select, });
+        try step_inner(m, th);
+    }
+    fn step_inner (m: *Machine, th: i8) !void {
+        if (m.todo_offset >= m.todo_list.items.len) {
+            return ;
+        }
+        const todo = m.todo_list.items[m.todo_offset];
+        m.todo_offset += 1;
+        const row1 = if (todo.row == 0) m.matrix_state.data_row - 1 else todo.row - 1;
+        const col1 = if (todo.col == 0) m.matrix_state.data_col - 1 else todo.col - 1;
+        const row2 = if (todo.row == m.matrix_state.data_row - 1) 0 else todo.row + 1;
+        const col2 = if (todo.col == m.matrix_state.data_col - 1) 0 else todo.col + 1;
+        const nxts: [4] Index = .{ 
+            .{ .col = col1, .row = row1, },
+            .{ .col = col2, .row = row1, },
+            .{ .col = col1, .row = row2, },
+            .{ .col = col2, .row = row2, },
+        };
+        const st = m.matrix_state.ref(todo.row, todo.col).?;
+        if (st.is_activated(th)) {
+            st.revert();
+        } else {
+            return ;
+        }
+        inline for (nxts) |n| {
+            const idx = n.row * m.matrix_state.data_col + n.col;
+            if (!m.visitor.isSet(idx)) {
+                m.visitor.set(idx);
+                try m.todo_list.append(m.allocator, n);
+            }
+        }
+    }
+    pub fn deinit (m: *Machine) void {
+        m.visitor.deinit (m.allocator);
+        m.activated.deinit (m.allocator);
+        m.todo_list.deinit (m.allocator);
+        m.matrix_state.deinit (m.allocator);
+    }
+};
